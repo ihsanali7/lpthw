8ed4c7284599a00c08f247a13547ad83c9ef9e4a
Ihsan Ali <ihsanali@Ihsans-MacBook-Pro.local>
2025-08-21 13:56:37 -0400

    Initial commit

---
+++ ex5.py
@@ -0,0 +1,18 @@
+name = 'Ihsan Ali'
+age = 22 # not a lie
+height = 72 # inches
+weight = 140 #lbs
+eyes = 'Black'
+teeth = 'White'
+hair = "Black"
+
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches tall.")
+print(f"He's {weight} pounds heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {eyes} and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
+
+# This line is tricky, try to get it exactly right
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
